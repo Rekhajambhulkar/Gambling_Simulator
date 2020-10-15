66950560a42930e96f5c60a853350e538c304c3b
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-10-15 11:50:03 +0530

    Update GamblingSimulator.java

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -4,7 +4,7 @@ public class GamblingSimulator
 {
 public static void main(String args[])
 {
-
+System.out.println("Welcome in GamblingSimulator Program");
 public static final stake=100;
 public static final bet=1;
 }
