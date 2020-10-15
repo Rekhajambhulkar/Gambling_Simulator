5d5300ba52a700b9c2d66bb830125ebe2a26a735
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-10-15 12:59:34 +0530

    ResignGame if more than 50% win

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -17,10 +17,34 @@ else {
 	System.out.println("Loss");
 }
 }
+public static void resignGame(){
+int cash = stake;
+int win=1;
+int take=150;
+int Goal=50;
+int range=50;
+while(cash < take  && cash > Goal){	
+if (win == range ) {
+	int random = (int) (Math.random() * 10 )% 2;
+	cash = cash + bet;
+}
+else {
+	cash = cash - bet;	
+}
+}
+if(stake == Goal)
+{
+	System.out.println("Win");
+}
+else {
+	System.out.println("Loose");
+}
+}
 public static void main(String args[])
 {
 System.out.println("Welcome in Gambling Simulator program");
 GamblingSimulator gam = new GamblingSimulator();
-gam.winORLoss();
+winORLoss();
+resignGame();
 }
 }
